a6e416d93da653616fe83048270a9a8028c4a7c4
Antonio Cesar Prieto <antonio.sistemas2013@gmail.com>
2026-08-21 10:46:48 -0300

    Adiciona busca e edição de residentes

--- src/residentes.py
+++ src/residentes.py
@@ -51,8 +51,54 @@ def cadastrar_residente(nome, cpf, cidade_origem):
     }
 
 def buscar_residente_por_cpf(cpf):
-    pass
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+        """
+        SELECT id, nome, cpf, cidade_origem, ativo
+        FROM residentes
+        WHERE cpf = ?
+        """,
+        (cpf,)
+    )
+
+    residente = cursor.fetchone()
+
+    conexao.close()
+
+    if residente is None:
+        return None
+
+    return {
+        "id": residente[0],
+        "nome": residente[1],
+        "cpf": residente[2],
+        "cidade_origem": residente[3],
+        "ativo": residente[4]
+    }
 
 
-def editar_residente():
-    pass
+def editar_residente(id_residente, nome, cpf, cidade_origem, ativo):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+        """
+        UPDATE residentes
+        SET nome = ?,
+            cpf = ?,
+            cidade_origem = ?,
+            ativo = ?
+        WHERE id = ?
+        """,
+        (nome, cpf, cidade_origem, ativo, id_residente)
+    )
+
+    conexao.commit()
+
+    alterado = cursor.rowcount
+
+    conexao.close()
+
+    return alterado > 0
