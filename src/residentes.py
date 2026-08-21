ed3a485b3b2a58db0262cc4a3562f4be46896d76
Antonio Cesar Prieto <antonio.sistemas2013@gmail.com>
2026-08-21 10:39:37 -0300

    Implementa cadastro de residentes

--- src/residentes.py
+++ src/residentes.py
@@ -1,6 +1,54 @@
-def cadastrar_residente():
-    pass
+from src.banco import conectar
 
+
+def cadastrar_residente(nome, cpf, cidade_origem):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+        """
+        SELECT id, nome, cpf, cidade_origem, ativo
+        FROM residentes
+        WHERE cpf = ?
+        """,
+        (cpf,)
+    )
+
+    residente = cursor.fetchone()
+
+    if residente:
+        conexao.close()
+        return {
+            "existe": True,
+            "id": residente[0],
+            "nome": residente[1],
+            "cpf": residente[2],
+            "cidade_origem": residente[3],
+            "ativo": residente[4]
+        }
+
+    cursor.execute(
+        """
+        INSERT INTO residentes (nome, cpf, cidade_origem)
+        VALUES (?, ?, ?)
+        """,
+        (nome, cpf, cidade_origem)
+    )
+
+    conexao.commit()
+
+    id_residente = cursor.lastrowid
+
+    conexao.close()
+
+    return {
+        "existe": False,
+        "id": id_residente,
+        "nome": nome,
+        "cpf": cpf,
+        "cidade_origem": cidade_origem,
+        "ativo": 1
+    }
 
 def buscar_residente_por_cpf(cpf):
     pass
